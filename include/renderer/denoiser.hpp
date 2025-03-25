2fa6e7cf56de862172b13d63455ba7da52d2badb
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2025-03-24 19:01:20 -0700

    Invoke denoiser

--- include/renderer/denoiser.hpp
+++ include/renderer/denoiser.hpp
@@ -8,6 +8,7 @@ class RealTimeDenoiser
 private:
 OptixDeviceContext context;
 OptixDenoiser denoiser;
+OptixDenoiserSizes denoiser_sizes;
 void* denoiser_state;
 void* denoiser_scratch;
 
@@ -15,5 +16,6 @@ public:
     RealTimeDenoiser(unsigned int width, unsigned int height);
     ~RealTimeDenoiser();
 
+    // input image buffers should be in CUDA device memory
     void denoise(const float* color, const float* albedo, const float* normal, const float* output, int width, int height);
 };
--- src/denoiser.cpp
+++ src/denoiser.cpp
@@ -44,7 +44,7 @@ RealTimeDenoiser::RealTimeDenoiser(unsigned int width, unsigned int height) {
 
     std::cout << "Setting up OptiX denoiser..." << std::endl;
     // Set up the denoiser
-    OptixDenoiserSizes denoiser_sizes;
+    // OptixDenoiserSizes denoiser_sizes;
     optixDenoiserComputeMemoryResources(denoiser, width, height, &denoiser_sizes);
 
     cudaMalloc(&denoiser_state, denoiser_sizes.stateSizeInBytes);
@@ -77,5 +77,63 @@ void RealTimeDenoiser::denoise(const float* color, const float* albedo, const fl
 {
     // Denoise the image using OptiX
     std::cout << "Denoising image with OptiX..." << std::endl;
+
+    OptixImage2D inputLayer[3] = {};
+
+    inputLayer[0].data = reinterpret_cast<CUdeviceptr>(color);
+    inputLayer[0].width = width;
+    inputLayer[0].height = height;
+    inputLayer[0].rowStrideInBytes = width * sizeof(float) * 3;
+    inputLayer[0].pixelStrideInBytes = sizeof(float) * 3;
+    inputLayer[0].format = OPTIX_PIXEL_FORMAT_FLOAT3;
+
+    // inputLayer[1].data = reinterpret_cast<CUdeviceptr>(albedo);
+    // inputLayer[1].width = width;
+    // inputLayer[1].height = height;
+    // inputLayer[1].rowStrideInBytes = width * sizeof(float) * 4;
+    // inputLayer[1].pixelStrideInBytes = sizeof(float) * 4;
+    // inputLayer[1].format = OPTIX_PIXEL_FORMAT_FLOAT4;
+
+    // inputLayer[2].data = reinterpret_cast<CUdeviceptr>(normal);
+    // inputLayer[2].width = width;
+    // inputLayer[2].height = height;
+    // inputLayer[2].rowStrideInBytes = width * sizeof(float) * 4;
+    // inputLayer[2].pixelStrideInBytes = sizeof(float) * 4;
+    // inputLayer[2].format = OPTIX_PIXEL_FORMAT_FLOAT4;
+
+    OptixImage2D outputLayer = {};
+    outputLayer.data = reinterpret_cast<CUdeviceptr>(output);
+    outputLayer.width = width;
+    outputLayer.height = height;
+    outputLayer.rowStrideInBytes = width * sizeof(float) * 3;
+    outputLayer.pixelStrideInBytes = sizeof(float) * 3;
+    outputLayer.format = OPTIX_PIXEL_FORMAT_FLOAT3;
+
+    OptixDenoiserParams params = {};
+    params.hdrIntensity = 0.0;
+    params.blendFactor = 0.0;
+
+    OptixDenoiserGuideLayer guideLayer = {};
+    // guideLayer.albedo = inputLayer[1];
+    // guideLayer.normal = inputLayer[2];
+    // guideLayer.albedo = 0;
+    // guideLayer.normal = 0;
+
+    OptixDenoiserLayer denoiserLayer = {};
+    denoiserLayer.input = inputLayer[0];
+    denoiserLayer.output = outputLayer;
+
+    optixDenoiserInvoke(denoiser, 
+        nullptr, 
+        &params, 
+        reinterpret_cast<CUdeviceptr>(denoiser_state), 
+        denoiser_sizes.stateSizeInBytes, 
+        &guideLayer, 
+        &denoiserLayer, 1, 0, 0, 
+        reinterpret_cast<CUdeviceptr>(denoiser_scratch), 
+        denoiser_sizes.withoutOverlapScratchSizeInBytes
+);
+
+    std::cout << "Image denoised successfully." << std::endl;
 
 }
